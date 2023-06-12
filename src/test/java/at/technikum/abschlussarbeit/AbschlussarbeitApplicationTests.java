fde29b65d813bad311ce38ccdc4935c0778ebe05
taqihassan <hassantaqi475@gmail.com>
2023-06-12 21:12:39 +0200

    Initial commit

---
+++ src/test/java/at/technikum/abschlussarbeit/AbschlussarbeitApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.abschlussarbeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbschlussarbeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
